3ffa5e6a64f1dbcd544e00aa0be9efec8b6ebfa3
Paul Tonning <paul.tonning@student.vvc.edu>
2020-02-20 15:20:16 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<vector>
 
 struct Course;
 
 struct Student
 {
+   std::string name;
+   std::vector<Course*> courses;
 };
 
-struct Course
+struct Course 
 {
+   std::string name;
+   std::vector<Student*> students;
 };
 
-int main() {
-  std::cout << "Hello World!\n";
+
+int main()
+{
+   Student s1;
+   Student *s2 = new Student;
+   s2->name = "Fred Flintstone";
+   Course *c1 = new Course;
+   c1->name = "CIS101";
+   c1->students.push_back(s2);
+
+   return 0;
 }
