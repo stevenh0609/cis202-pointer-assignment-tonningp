cc3d659a15ac5d6416da23531784a8e194f1026b
stevenh0609 <61297336+stevenh0609@users.noreply.github.com>
2020-02-20 17:24:27 -0800

    finished pointer project version 1

--- main.cpp
+++ main.cpp
@@ -1,30 +1,77 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
 
 struct Course;
 
-struct Student
-{
-   std::string name;
-   std::vector<Course*> courses;
+struct Student {
+  std::string name;
+  std::vector<std::shared_ptr<Course>> courses;
 };
 
-struct Course 
-{
-   std::string name;
-   std::vector<Student*> students;
+struct Course {
+  std::string name;
+  std::vector<std::shared_ptr<Student>> students;
 };
 
+void print_student(std::shared_ptr<Student> s) {
+  std::cout << s->name << "\n";
+  for (auto ptr : s->courses) {
+    std::cout << ptr->name << "\n";
+  }
+  std::cout << "\n";
+}
 
-int main()
-{
-   Student s1;
-   Student *s2 = new Student;
-   s2->name = "Fred Flintstone";
-   Course *c1 = new Course;
-   c1->name = "CIS101";
-   c1->students.push_back(s2);
+void print_course(std::shared_ptr<Course> c) {
+  std::cout << c->name << "\n";
+  for (auto ptr : c->students) {
+    std::cout << ptr->name << "\n";
+  }
+  std::cout << "\n";
+}
 
-   return 0;
+void enroll(std::shared_ptr<Student> s, std::shared_ptr<Course> c) {
+  s->courses.push_back(c);
+  c->students.push_back(s);
+}
+
+void enroll_mod2(std::vector<std::shared_ptr<Student>> students, std::vector<std::shared_ptr<Course>> courses) {
+  for (auto i = 0u; i < students.size(); ++i) {
+    for (auto j = 0u; j < courses.size(); ++j) {
+      if (rand() % 2) {
+        enroll(students[i], courses[j]);
+      }
+    }
+  }
+}
+
+int main() {
+  srand(time(NULL));
+
+  auto student_names = { "Matthew", "Sean", "Leon", "Daphne", "Mark", "Luke", "John", "Ringo", "Starr", "Stephanie", "Tiffany" };
+  auto course_names = { "CIS101", "CIS201", "CIS202" };
+  
+  std::vector<std::shared_ptr<Student>> students;
+  std::vector<std::shared_ptr<Course>> courses;
+
+  for (const auto & sn : student_names) {
+    students.push_back(std::make_shared<Student>());
+    students.back()->name = sn;
+  }
+  for (const auto & cn : course_names) {
+    courses.push_back(std::make_shared<Course>());
+    courses.back()->name = cn;
+  }
+
+  enroll_mod2(students, courses);
+
+  for (const auto & s : students) {
+      print_student(s);
+    }
+  for (const auto & c : courses) {
+      print_course(c);
+    }
+
+  return 0;
 }
